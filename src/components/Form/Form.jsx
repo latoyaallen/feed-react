0eeed3190c883cb6d9a64f7ae9a633718e5e2046
latoyanallen <me@allen-labs.com>
2022-12-01 17:11:44 -0500

    Add Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '../Button';
+
+const Form = () => {
+  return (
+    <div>
+      <Button>
+        Another Button
+      </Button>
+    </div>
+  );
+}
+
+export default Form;
